ef63f074c03c159392b4eadedbce5f549dced38e
Kristin <kristindyulgeryan@abv.bg>
2025-04-07 16:07:15 +0300

    Auto-submit (timeout) after 10 seconds via `handleSelectAnswer(null)`

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -3,7 +3,9 @@ import React, { useState, useEffect } from "react";
 export default function QuestionTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
-  setTimeout(onTimeout, timeout);
+  useEffect(() => {
+    setTimeout(onTimeout, timeout);
+  }, [timeout, onTimeout]);
 
   useEffect(() => {
     setInterval(() => {
@@ -11,5 +13,5 @@ export default function QuestionTimer({ timeout, onTimeout }) {
     }, 100);
   }, []);
 
-  return <progress id="question-time" />;
+  return <progress id="question-time" max={timeout} value={remainingTime} />;
 }
